591612126d6377fe0825fd900fd2683e4f8b7d5d
Shim MunSeong <Puterism.K@gmail.com>
2021-09-09 15:08:26 +0900

    chore: 불필요한 주석 코드 삭제

--- functions/index.js
+++ functions/index.js
@@ -10,133 +10,49 @@ admin.initializeApp({
 
 exports.notice = require('./src/notice');
 
-exports.createOrder = functions
-  .region('asia-northeast2')
-  .https.onCall(async (data, context) => {
-    const serverTimestamp = admin.firestore.FieldValue.serverTimestamp();
-    const {
-      order_number,
-      items,
-      total,
-      subtotal,
-      tax_rate,
-      tracking_number,
-      status,
-    } = data;
-    const { token, uid } = context.auth;
-    const { name, email } = token;
-    const clientUser = {
-      client_name: name,
-      client_account_email: email,
-      client_account_uid: uid,
-    };
+exports.createOrder = functions.region('asia-northeast2').https.onCall(async (data, context) => {
+  const serverTimestamp = admin.firestore.FieldValue.serverTimestamp();
+  const { order_number, items, total, subtotal, tax_rate, tracking_number, status } = data;
+  const { token, uid } = context.auth;
+  const { name, email } = token;
+  const clientUser = {
+    client_name: name,
+    client_account_email: email,
+    client_account_uid: uid,
+  };
 
-    // TODO: items의 quantity 체크 w/ transaction
-    // https://firebase.google.com/docs/firestore/manage-data/transactions?hl=ko
-    // https://stackoverflow.com/questions/57653308/firestore-transaction-update-multiple-collections-in-a-single-transaction
-    // https://thecloudfunction.com/blog/atomic-operations-with-firebase/
-    // need
-    // return admin
-    //   .firestore()
-    //   .runTransaction((transaction) => {
-    //     items.forEach((item) => {
-    //       const { orderQuantity } = item;
-    //       const quantityDecrement = admin.firestore.FieldValue.increment(-orderQuantity);
-    //       const itemRef = admin.firestore().collection('item').doc(item.id);
-    //       const itemDoc = transaction.get(itemRef);
-    //       // TODO: itemDoc.data is not a function
-    //       const itemData = itemDoc.data();
-    //       const { quantity } = itemData;
-    //       if (quantity >= orderQuantity) {
-    //         transaction.update(itemRef, {
-    //           quantity: quantityDecrement,
-    //         });
-    //       }
-    //     });
+  return admin
+    .firestore()
+    .collection('order')
+    .doc(order_number)
+    .set({
+      ...clientUser,
+      order_number: order_number,
+      items: items,
+      total: total,
+      subtotal: subtotal,
+      tax_rate: tax_rate,
+      tracking_number: tracking_number,
+      status: status,
+      created_at: serverTimestamp,
+    })
+    .then(() => {
+      return order_number;
+    })
+    .catch((err) => {
+      console.error('Error create order: ', err);
+      throw new functions.https.HttpsError('internal', err);
+    });
+});
 
-    //   })
-    return admin
-      .firestore()
-      .collection('order')
-      .doc(order_number)
-      .set({
-        ...clientUser,
-        order_number: order_number,
-        items: items,
-        total: total,
-        subtotal: subtotal,
-        tax_rate: tax_rate,
-        tracking_number: tracking_number,
-        status: status,
-        created_at: serverTimestamp,
-      })
-      .then(() => {
-        return order_number;
-      })
-      .catch((err) => {
-        console.error('Error create order: ', err);
-        throw new functions.https.HttpsError('internal', err);
-      });
-  });
-
-// exports.onCreateOrder = functions.firestore.document('order/{id}').onCreate((snap, context) => {
-//   let newOrder = snap.data();
-//   let total = 0;
-//   const { items } = newOrder;
-//   const batch = firestore.batch();
-
-//   items.forEach((item) => {
-//     const { id, price, quantity, orderQuantity } = item;
-//     const itemRef = firestore.collection('item').doc(id);
-//     const quantityDecrement = admin.firestore.FieldValue.increment(-orderQuantity);
-//     total += price * orderQuantity;
-
-//     if (quantity < orderQuantity) {
-//       console.error('quantity < orderQuantity', quantity < orderQuantity);
-//       throw new Error('Quantity is less than ordered quantity');
-//     }
-
-//     batch.update(itemRef, {
-//       // quantity: quantity - orderQuantity,
-//       quantity: quantityDecrement,
-//     });
-//     // itemRef
-//     //   .update({
-//     //     quantity: quantity - orderQuantity,
-//     //   })
-//     //   .then((response) => {
-//     //     console.log('Document successfully updated!', response);
-//     //     return true;
-//     //   })
-//     //   .catch((err) => {
-//     //     // The document probably doesn't exist.
-//     //     console.error('Error updating document: ', err);
-//     //     throw err;
-//     //   });
-//   });
-
-//   return batch
-//     .commit()
-//     .then(() => {
-//       console.log('Batch successfully updated!');
-//       return true;
-//     })
-//     .catch((err) => {
-//       console.error('Error batch updating document: ', err);
-//       throw err;
-//     });
-// });
-
-exports.createAccount = functions
-  .region('asia-northeast2')
-  .https.onCall((data) => {
-    return admin
-      .auth()
-      .createUser(data)
-      .catch((error) => {
-        throw new functions.https.HttpsError('internal', error.message);
-      });
-  });
+exports.createAccount = functions.region('asia-northeast2').https.onCall((data) => {
+  return admin
+    .auth()
+    .createUser(data)
+    .catch((error) => {
+      throw new functions.https.HttpsError('internal', error.message);
+    });
+});
 
 exports.onCreateAccount = functions
   .region('asia-northeast2')
@@ -165,47 +81,43 @@ exports.onCreateAccount = functions
       });
   });
 
-exports.setAdmin = functions
-  .region('asia-northeast2')
-  .https.onCall((email, context) => {
-    if (context.auth.token.admin !== true) {
-      throw new functions.https.HttpsError(
-        'permission-denied',
-        'Request not authorized. User must be a admin to fulfill request.',
-      );
-    }
+exports.setAdmin = functions.region('asia-northeast2').https.onCall((email, context) => {
+  if (context.auth.token.admin !== true) {
+    throw new functions.https.HttpsError(
+      'permission-denied',
+      'Request not authorized. User must be a admin to fulfill request.',
+    );
+  }
 
-    return grantAdminRole(email)
-      .then(() => {
-        return {
-          result: `Request fulfilled! ${email} is now a admin.`,
-        };
-      })
-      .catch((err) => {
-        throw new functions.https.HttpsError('unknown', err);
-      });
-  });
+  return grantAdminRole(email)
+    .then(() => {
+      return {
+        result: `Request fulfilled! ${email} is now a admin.`,
+      };
+    })
+    .catch((err) => {
+      throw new functions.https.HttpsError('unknown', err);
+    });
+});
 
-exports.unsetAdmin = functions
-  .region('asia-northeast2')
-  .https.onCall((email, context) => {
-    if (context.auth.token.admin !== true) {
-      throw new functions.https.HttpsError(
-        'permission-denied',
-        'Request not authorized. User must be a admin to fulfill request.',
-      );
-    }
+exports.unsetAdmin = functions.region('asia-northeast2').https.onCall((email, context) => {
+  if (context.auth.token.admin !== true) {
+    throw new functions.https.HttpsError(
+      'permission-denied',
+      'Request not authorized. User must be a admin to fulfill request.',
+    );
+  }
 
-    return grantUserRole(email)
-      .then(() => {
-        return {
-          result: `Request fulfilled! ${email} is now a admin.`,
-        };
-      })
-      .catch((err) => {
-        throw new functions.https.HttpsError('unknown', err);
-      });
-  });
+  return grantUserRole(email)
+    .then(() => {
+      return {
+        result: `Request fulfilled! ${email} is now a admin.`,
+      };
+    })
+    .catch((err) => {
+      throw new functions.https.HttpsError('unknown', err);
+    });
+});
 
 async function grantAdminRole(email) {
   const user = await admin.auth().getUserByEmail(email);
@@ -227,46 +139,42 @@ async function grantUserRole(email) {
   });
 }
 
-exports.disableUser = functions
-  .region('asia-northeast2')
-  .https.onCall((email, context) => {
-    if (context.auth.token.admin !== true) {
-      throw new functions.https.HttpsError(
-        'permission-denied',
-        'Request not authorized. User must be a admin to fulfill request.',
-      );
-    }
-    return disableUserPromise(email)
-      .then(() => {
-        return {
-          result: `Request fulfilled! ${email} is disabled now.`,
-        };
-      })
-      .catch((err) => {
-        throw new functions.https.HttpsError('unknown', err);
-      });
-  });
+exports.disableUser = functions.region('asia-northeast2').https.onCall((email, context) => {
+  if (context.auth.token.admin !== true) {
+    throw new functions.https.HttpsError(
+      'permission-denied',
+      'Request not authorized. User must be a admin to fulfill request.',
+    );
+  }
+  return disableUserPromise(email)
+    .then(() => {
+      return {
+        result: `Request fulfilled! ${email} is disabled now.`,
+      };
+    })
+    .catch((err) => {
+      throw new functions.https.HttpsError('unknown', err);
+    });
+});
 
-exports.enableUser = functions
-  .region('asia-northeast2')
-  .https.onCall((email, context) => {
-    if (context.auth.token.admin !== true) {
-      throw new functions.https.HttpsError(
-        'permission-denied',
-        'Request not authorized. User must be a admin to fulfill request.',
-      );
-    }
+exports.enableUser = functions.region('asia-northeast2').https.onCall((email, context) => {
+  if (context.auth.token.admin !== true) {
+    throw new functions.https.HttpsError(
+      'permission-denied',
+      'Request not authorized. User must be a admin to fulfill request.',
+    );
+  }
 
-    return enableUserPromise(email)
-      .then(() => {
-        return {
-          result: `Request fulfilled! ${email} is enabled now.`,
-        };
-      })
-      .catch((err) => {
-        throw new functions.https.HttpsError('unknown', err);
-      });
-  });
+  return enableUserPromise(email)
+    .then(() => {
+      return {
+        result: `Request fulfilled! ${email} is enabled now.`,
+      };
+    })
+    .catch((err) => {
+      throw new functions.https.HttpsError('unknown', err);
+    });
+});
 
 async function disableUserPromise(email) {
   const user = await admin.auth().getUserByEmail(email);
@@ -287,35 +195,3 @@ async function enableUserPromise(email) {
     disabled: false,
   });
 }
-
-// exports.getNotices = functions.region('asia-northeast2').https.onCall(() => {
-//   return admin
-//     .firestore()
-//     .collection('notice')
-//     .orderBy('created_at', 'desc')
-//     .get()
-//     .then((querySnapshot) => {
-//       const result = [];
-//       querySnapshot.forEach((doc) => {
-//         const data = doc.data();
-//         const created_at = data.created_at.toDate() + '';
-//         result.push({
-//           id: doc.id,
-//           title: data.title,
-//           data: data.data,
-//           created_at: created_at,
-//         });
-//       });
-//       console.log(result);
-//       return result;
-//     })
-//     .catch((err) => {
-//       throw new functions.https.HttpsError('internal', err);
-//     });
-// });
-
-// exports.changePassword = functions
-//   .region('asia-northeast2')
-//   .https.onCall((currentPassword, newPassword) => {
-
-//   });
